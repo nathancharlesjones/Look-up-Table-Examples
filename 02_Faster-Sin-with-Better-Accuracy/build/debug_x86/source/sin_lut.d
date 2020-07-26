2df2f1ecb58686d95e517d1827b6b74cbcd9917b
Nathan Jones <nathancharlesjones@gmail.com>
2020-07-25 21:07:15 -0400

    Updated files.

--- 02_Faster-Sin-with-Better-Accuracy/build/debug_x86/source/sin_lut.d
+++
@@ -1,6 +0,0 @@
-build/debug_x86/source/sin_lut.o: source/sin_lut.c include/sin_lut.h \
- include/fixed_point.h
-
-include/sin_lut.h:
-
-include/fixed_point.h:
--- 02_Faster-Smaller-Sin-with-Better-Accuracy/build/debug_STM32F1/hardware/source/STM32F1/source/startup_stm32f103xb.d
+++
@@ -1,2 +0,0 @@
-build/debug_STM32F1/hardware/source/STM32F1/source/startup_stm32f103xb.o: \
- hardware/source/STM32F1/source/startup_stm32f103xb.s
